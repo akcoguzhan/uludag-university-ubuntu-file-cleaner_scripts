f2faae2bd37b8486c758145200f1690e5474cf64
akcoguzhan <akcomakoguzhan@gmail.com>
2022-06-29 00:26:57 +0300

    eng descriptions

--- .boot_logger/boot_log_creater.py
+++ .boot_logger/boot_log_creater.py
@@ -1,58 +1,61 @@
 #!/usr/bin/python3
-import os 
+import os
 import subprocess
 from datetime import datetime
+
 
 def os_commmand_with_return(command, decode="utf-8"):
     cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     return cmd.stdout.read().decode(decode)
 
+
 def main():
-    # açılışın tarih ve saat bilgisi
+    # the date and time information of startup
     boot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # açılış yılı
-    current_year = os_commmand_with_return('date +%Y').strip()
+    # startup year
+    current_year = os_commmand_with_return("date +%Y").strip()
 
-
-    # yıllara göre log işlemi yapılması için
+    # for logging by years
     # /Cleaner/boot_logs/2021/boot_logs.txt
-    # şeklinde kayıtlar oluşturulur
-    # ilgili yıl içersineki tüm kayıtlar aynı belge içerisine kaydedilir
-    os.chdir('/Cleaner')
+    # records are created as above
+    # all records in the related year are recorded in the same document.
+    os.chdir("/Cleaner")
 
-    if not os.path.exists('boot_logs'):
-        os.mkdir('boot_logs')
+    if not os.path.exists("boot_logs"):
+        os.mkdir("boot_logs")
 
-    os.chdir('boot_logs')
+    os.chdir("boot_logs")
 
     log_by_years = [f.path for f in os.scandir() if f.is_dir()]
 
-    if str('./' + current_year) not in log_by_years:
+    if str("./" + current_year) not in log_by_years:
         os.mkdir(current_year)
 
     os.chdir(current_year)
-        
-    
-    f = open('boot_logs.txt', 'a')
 
-    # yeni yılın ilk kaydı oluşturulmada önce dosyanın boyutu kontrol edilir
-    # eğer dosya yeni oluşturulmuş ise boyutu 0 olacaktır. bu sayede yeni yıl
-    # kontrolü yapılmış olur. 
-    if os.path.getsize('boot_logs.txt') == 0:
-        # önceki yıl kayıtlarının sonuncusunu dosyanın başına ekleriz
-        # bu sayede junk_cleaner.py içerisinde değişiklik yapmaya gerek kalmadan
-        # script yeni yıla uygun hale getirilmiş olur
+    f = open("boot_logs.txt", "a")
+
+    # file size is checked before the first record of the new year is created
+    # if the file is newly created, its size will be 0 byte. In this way, the new year control is made.
+    if os.path.getsize("boot_logs.txt") == 0:
+        # we add the last of the previous year records to the beginning of the file
+        # In this way, the script is adapted to the new year
+        # without the need to make changes in junk_cleaner.py.
         try:
-            previous_year_last_boot_log = open('/Cleaner/boot_logs/' + str(int(current_year) - 1) + '/boot_logs.txt')
-            previous_year_last_boot_record = str(previous_year_last_boot_log.readlines()[-1]).strip()
+            previous_year_last_boot_log = open(
+                "/Cleaner/boot_logs/" + str(int(current_year) - 1) + "/boot_logs.txt"
+            )
+            previous_year_last_boot_record = str(
+                previous_year_last_boot_log.readlines()[-1]
+            ).strip()
             previous_year_last_boot_log.close()
-            f.write(previous_year_last_boot_record + '\n')
+            f.write(previous_year_last_boot_record + "\n")
         except:
             pass
 
-
-    f.write(boot_time + '\n')
+    f.write(boot_time + "\n")
     f.close()
+
 
 if __name__ == "__main__":
     main()
--- .junk_cleaner/junk_cleaner.py
+++ .junk_cleaner/junk_cleaner.py
@@ -7,43 +7,48 @@ from sys import stdout
 from datetime import datetime
 import time
 
-# isletim sisteminden donusu olan fonksiyonlar icin string donusu yapar
+# Returns strings for functions that have any return from the operating system
 def os_command_with_return(command, decode="utf-8"):
     cmd = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
     cmd.wait()
     return cmd.stdout.read().decode(decode)
 
-# bir onceki bilgisayar acilis zaman bilgisini doner
+
+# returns the previous computer boot time information
 def get_previous_boot_time():
-    current_year = os_command_with_return('date +%Y').strip()
+    current_year = os_command_with_return("date +%Y").strip()
     try:
-        logs_file = open('/Cleaner/boot_logs/' + current_year + '/boot_logs.txt')
+        logs_file = open("/Cleaner/boot_logs/" + current_year + "/boot_logs.txt")
         previous_boot_time = str(logs_file.readlines()[-2]).strip()
         return previous_boot_time
     except:
         exit()
 
-# bilgisayarin acilis zaman bilgisini doner
+
+# Returns the computer's boot time information
 def get_current_boot_time():
     current_boot_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     return current_boot_time
-    
-# silinmesi istenen ve istenmeyen dosyalari iki farkli liste olarak doner
-def get_junk_files(includedDirectories="/Cleaner/.junk_cleaner/include.txt", excludedDirectories="/Cleaner/.junk_cleaner/exclude.txt"):
+
+
+# It returns the wanted and unwanted files as two different lists.
+def get_junk_files(
+    includedDirectories="/Cleaner/.junk_cleaner/include.txt",
+    excludedDirectories="/Cleaner/.junk_cleaner/exclude.txt",
+):
     to_look = []
     to_exclude = []
 
-    with open(includedDirectories, 'r') as file:
+    with open(includedDirectories, "r") as file:
         for line in file:
             if not line.startswith("#") and not line.startswith("\n"):
                 to_look.append(line.strip())
-    with open(excludedDirectories, 'r') as file:
+    with open(excludedDirectories, "r") as file:
         for line in file:
             if not line.startswith("#") and not line.startswith("\n"):
                 to_exclude.append(line.strip())
 
     return to_look, to_exclude
-
 
 
 def clean_directories():
@@ -55,7 +60,15 @@ def clean_directories():
     junk_files = []
 
     for dir in dirs_to_check:
-        command = 'find ' + str(dir) + ' -newermt "' + str(previous_boot_time) + '" ! -newermt "' + str(current_boot_time) + '"'
+        command = (
+            "find "
+            + str(dir)
+            + ' -newermt "'
+            + str(previous_boot_time)
+            + '" ! -newermt "'
+            + str(current_boot_time)
+            + '"'
+        )
 
         for ignore in dirs_to_ignore:
             if str(ignore).find(str(dir)) != -1:
@@ -64,23 +77,23 @@ def clean_directories():
         files_to_remove = os_command_with_return(command).split("\n")
 
         try:
-            files_to_remove.remove('')
+            files_to_remove.remove("")
         except ValueError:
             pass
         junk_files.extend(files_to_remove)
-    
+
     for dir in dirs_to_check:
         try:
-            junk_files.remove(dir)       
+            junk_files.remove(dir)
         except ValueError:
             pass
-        
+
     for dir in dirs_to_ignore:
         try:
             junk_files.remove(dir)
         except ValueError:
             pass
-    
+
     backup_files(previous_boot_time, junk_files)
 
     if len(junk_files) != 0:
@@ -88,62 +101,73 @@ def clean_directories():
             os.system('echo rm -r "' + file + '" >> /Cleaner/deleted_files.txt')
             os.system('rm -r "' + file + '"')
 
+
 def backup_files(backup_boot_time, junk_files):
-    if not os.path.exists('/Cleaner/.backups'):
-        os.mkdir('/Cleaner/.backups')
-    backup_count = os.listdir('/Cleaner/.backups')
+    if not os.path.exists("/Cleaner/.backups"):
+        os.mkdir("/Cleaner/.backups")
+    backup_count = os.listdir("/Cleaner/.backups")
 
     if len(junk_files) == 0:
         return
     else:
         try:
-            os.mkdir('/Cleaner/.backups/' + str(backup_boot_time))
+            os.mkdir("/Cleaner/.backups/" + str(backup_boot_time))
         except FileExistsError:
             return
-        
+
         flag = False
         will_be_removed = []
         if len(backup_count) == 6:
             flag = True
-            with open('/Cleaner/.backups/last_backup_time.txt', 'r') as log:
+            with open("/Cleaner/.backups/last_backup_time.txt", "r") as log:
                 last_backup_time = log.readline().strip()
                 os.system('rm -r "/Cleaner/.backups/' + last_backup_time + '"')
-        
-            f = open('/Cleaner/.backups/last_backup_time.txt', 'r')
+
+            f = open("/Cleaner/.backups/last_backup_time.txt", "r")
             will_be_removed = f.readlines()[1:]
             f.close()
             os.system('rm -r "/Cleaner/.backups/last_backup_time.txt"')
 
         for junk in junk_files:
-            # dosya dizin ve isim bilgisi tek bir degiskende tutuldugu icin
-            # rsplit ile tek bir "/" karakteri ile bolme yapilirsa
-            # /home/test/junk.txt >> /home/test ve junk.txt olarak ikiye ayrilir
-            # soldan yapilamaz cunku dosyanin dizinine gelene kadar kac klasorden 
-            # gecildigi bilinmiyor (/ karakter sayisi degiskenlik gosterir)
-            # find "/home/ogrenci/Masaustu" -name "test.txt" -exec cp --parents \{\} "/Cleaner/.backups/2022-03-01" \;
-            path = junk.rsplit('/', 1)[0]
-            file = junk.rsplit('/', 1)[1]
-            cmd = 'find ' + '"' + path + '"' + ' -name "' + file + '" -type f -exec cp -r --parents \\{\\} "/Cleaner/.backups/' + str(backup_boot_time) + '" \\;'
+            # Since the file directory and name information is kept in a single variable,
+            # the rsplit command is used for the "/" character,
+            # it will split into /home/test/junk.txt >> /home/test and junk.txt.
+            # The operation cannot be done from the left because it is unknown
+            # how many folders are passed until the directory of the file
+            # (the number of the "/" sign varies)
+            path = junk.rsplit("/", 1)[0]
+            file = junk.rsplit("/", 1)[1]
+            cmd = (
+                "find "
+                + '"'
+                + path
+                + '"'
+                + ' -name "'
+                + file
+                + '" -type f -exec cp -r --parents \\{\\} "/Cleaner/.backups/'
+                + str(backup_boot_time)
+                + '" \\;'
+            )
             os.system(cmd)
-        
-        backup_log = open('/Cleaner/.backups/last_backup_time.txt', 'a+')
+
+        backup_log = open("/Cleaner/.backups/last_backup_time.txt", "a+")
         if flag:
             backup_log.writelines(will_be_removed)
-        backup_log.write(backup_boot_time + '\n')
+        backup_log.write(backup_boot_time + "\n")
         backup_log.close()
 
-# root tarafından silme işlemi yapıldığı için dizinlerin sahipliği
-# root hesabına geçmektedir. ogrenci hesabı tarafından herhangi bir 
-# dosya için silme işleminin yapılabilmesi için, Trash ve alt dizinlerin
-# sahipliğinin veya yazma ve okuma izinlerinin öğrenci için de tekrardan
-# aktif edilmesi gerekmektedir
+
+# Since the deletion is performed by root, the ownership of the
+# directories passes to the root account. In order for the student account
+# to be able to delete any file, ehe ownership (write and read permissions)
+# of the trash and subdirectories must be transferred to the student account.
 def clear_trash():
-    os.system('rm -rf /home/ogrenci/.local/share/Trash/*')
-    os.system('chmod -R o+rw /home/ogrenci/.local/share/Trash')
-    os.system('chmod -R o+rw /home/ogrenci/.local/share/Trash/files')
-    os.system('chmod -R o+rw /home/ogrenci/.local/share/Trash/expunged')
-    os.system('chmod -R o+rw /home/ogrenci/.local/share/Trash/info')
-    
+    os.system("rm -rf /home/ogrenci/.local/share/Trash/*")
+    os.system("chmod -R o+rw /home/ogrenci/.local/share/Trash")
+    os.system("chmod -R o+rw /home/ogrenci/.local/share/Trash/files")
+    os.system("chmod -R o+rw /home/ogrenci/.local/share/Trash/expunged")
+    os.system("chmod -R o+rw /home/ogrenci/.local/share/Trash/info")
+
 
 def main():
     clean_directories()
